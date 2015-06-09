64be205f19c8c73e6c021a05d03c6ab58e598202
Ryan Dandrow <RyanDandrow@gmail.com>
2015-06-09 10:50:03 -0500

    express routing and updated views layout

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./http/routes');
 
 var app = express();
 require('./bootstrap/middleware')(app);
 require('./bootstrap/views')(app);
 
-
 app.use('/', routes);
-app.use('/users', users);
 
 require('./bootstrap/errors')(app);
 
---
+++ http/routes.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+	router = express.Router();
+
+	homeResource = require('./resources/home'),
+	userResource = require('./resources/users'),
+
+router.use('./', homeResource);
+router.use('./users', userResource);
+
+module.exports = router;
