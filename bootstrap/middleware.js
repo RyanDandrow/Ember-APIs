bfc44019b6d01bc82083b8b4fd28304dc3e33f50
Ryan Dandrow <RyanDandrow@gmail.com>
2015-06-12 14:01:30 -0500

    fixed css issue for news-article pages, refactored news-article code, added bower method-override, added delete functionality for news articles

--- bootstrap/middleware.js
+++ bootstrap/middleware.js
@@ -1,10 +1,19 @@
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var override = require('method-override');
 
 module.exports = function (app) {
 	app.use(logger('dev'));
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(cookieParser());
+	app.use(override(function(req, res){
+  		if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    		// look in urlencoded POST bodies and delete it
+    		var method = req.body._method
+    		delete req.body._method
+ 	  		return method
+  		}
+	}));
 };
--- http/resources/news-articles.js
+++ http/resources/news-articles.js
@@ -3,69 +3,83 @@ var router = express.Router();
 var mongoose = require('mongoose'),
 	NewsArticle = mongoose.model('NewsArticle');
 
-	/* GET all news articles in a table */
-	router.get('/', function (req, res, next) {
-		NewsArticle.find().exec()
-			.then(function(newsArticles) {
-				var data = {
-					title: 'News Articles',
-					newsArticles: newsArticles
-				};
+var baseUrl = '/news-articles',
+	urlHelper = require('./../helpers/url')(baseUrl);
 
-				res.render('news-articles/index', data);
+var prepareData = function(title, data) {
+	data.title = title;
+	data.urlHelper = urlHelper;
+
+	return data;
+}
+
+/* GET all news articles in a table */
+router.get('/', function (req, res) {
+	NewsArticle.find().exec()
+		.then(function(newsArticles) {
+			var data = prepareData('News Articles', {
+				newsArticles: newsArticles
 			});
+
+			res.render('news-articles/index', data);
+		});
+});
+
+/* GET form to create news article. */
+router.get('/new', function (req, res) {
+	var data = prepareData('News Article - New', {
+		newsArticle: new NewsArticle()
 	});
 
-	/* GET form to create news article. */
-	router.get('/new', function (req, res) {
-		var data = {
-			title: 'News Articles - New',
-			newsArticle: new NewsArticle()
-		};
+	res.render('news-articles/create', data);
+});
 
-		res.render('news-articles/create', data);
-	});
+/* POST create news article from form data. */
+router.post('/', function (req, res) {
+	NewsArticle.create(req.body)
+		.then(function() {
+			res.redirect('/news-articles');
+		})
+});
 
-	/* POST create news article from form data. */
-	router.post('/', function (req, res) {
-		NewsArticle.create(req.body)
-			.then(function() {
-				res.redirect('/news-articles');
-			})
-	});
-
-	/* GET a detailed view of a news article. */
-	router.get('/:id', function (req, res) {
-		NewsArticle.findById(req.params.id).exec()
-			.then(function(newsArticle) {
-				var data = {
-					title: 'News Articles - ' + newsArticle.title,
-					newsArticle: newsArticle
-				};
-
-				res.render('news-articles/show', data);
+/* GET a detailed view of a news article. */
+router.get('/:id', function (req, res) {
+	NewsArticle.findById(req.params.id).exec()
+		.then(function(newsArticle) {
+			var data = prepareData('News Articles - ' + newsArticle.title, {
+				newsArticle: newsArticle
 			});
-	});
 
-	/* GET edit form for a news article. */
-	router.get('/:id/edit', function (req, res) {
-		NewsArticle.findById(req.params.id).exec()
-			.then(function(newsArticle) {
-				var data = {
-					title: 'News Articles - Edit - ' + newsArticle.title,
-					newsArticle: newsArticle
-				};
+			res.render('news-articles/show', data);
+		});
+});
 
-				res.render('news-articles/edit', data);
+/* GET edit form for a news article. */
+router.get('/:id/edit', function (req, res) {
+	NewsArticle.findById(req.params.id).exec()
+		.then(function(newsArticle) {
+			var data = prepareData('News Articles - Edit - ' + newsArticle.title, {
+				newsArticle: newsArticle
 			});
-	});
 
-	/* GET edit form for a news article. */
-	router.post('/:id', function (req, res) {
-		NewsArticle.findByIdAndUpdate(req.params.id, req.body).exec()
-			.then(function(newsArticle) {
-				res.redirect('news-articles');
-			});
-	});
+			res.render('news-articles/edit', data);
+		});
+});
+
+/* PUT save updates to a news article. */
+router.put('/:id', function (req, res) {
+	NewsArticle.findByIdAndUpdate(req.params.id, req.body).exec()
+		.then(function(newsArticle) {
+			res.redirect(urlHelper.index());
+		});
+});
+
+/* GET edit form for a news article. */
+router.delete('/:id', function (req, res) {
+	NewsArticle.findByIdAndRemove(req.params.id).exec()
+		.then(function(newsArticle) {
+			res.redirect(urlHelper.index());
+		});
+});
 
 module.exports = router;
